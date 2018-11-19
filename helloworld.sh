4baf11bc7531bcd607a191280fc80ee05713bab6
PyJan <jan.svarcbach@gmail.com>
2018-11-19 23:02:08 +0100

    first shell script

---
+++ helloworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'hello world' # print that
+# <chmod +x helloworld.sh> in order to make it executable
+# call <bash helloworld.sh> or <./helloworld.sh>
+echo 'another one'
+echo and one more
